97edaf5194d9749e1fc430256477e0d0ce3d8d50
Dr J T Andrews <108255180+jtandrewss@users.noreply.github.com>
2026-08-07 16:43:46 +0530

    Fetch data from external API and create cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+const API =
+"https://script.google.com/macros/s/AKfycbx5qhCgQVVV6MSXEnT0hcNnkgmrIhpQy4x5XuDRzMGd20vZX0Ytnkc1QitM9gyqAvdt/exec";
 
+fetch(API)
+
+.then(response=>response.json())
+
+.then(data=>{
+
+    createCards(data);
+
+});
